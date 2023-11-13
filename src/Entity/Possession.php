f6fbeed2dccce6913a2de9d3b7f3ff853dceb721
nesrine ishak <nesrineissac@gmail.com>
2023-11-13 14:16:16 +0100

    final commit

---
+++ src/Entity/Possession.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PossessionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+
+#[ApiResource]
+#[ORM\Entity(repositoryClass: PossessionRepository::class)]
+class Possession
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $valeur = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $type = null;
+
+    #[ORM\ManyToOne(inversedBy: 'iduser')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $iduser = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getValeur(): ?float
+    {
+        return $this->valeur;
+    }
+
+    public function setValeur(float $valeur): static
+    {
+        $this->valeur = $valeur;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getIduser(): ?User
+    {
+        return $this->iduser;
+    }
+
+    public function setIduser(?User $iduser): static
+    {
+        $this->iduser = $iduser;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $tel = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birthDate = null;
+
+    #[ORM\OneToMany(mappedBy: 'iduser', targetEntity: Possession::class)]
+    private Collection $iduser;
+
+    public function __construct()
+    {
+        $this->iduser = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(string $tel): static
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birthDate): static
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Possession>
+     */
+    public function getIduser(): Collection
+    {
+        return $this->iduser;
+    }
+
+    public function addIduser(Possession $iduser): static
+    {
+        if (!$this->iduser->contains($iduser)) {
+            $this->iduser->add($iduser);
+            $iduser->setIduser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIduser(Possession $iduser): static
+    {
+        if ($this->iduser->removeElement($iduser)) {
+            // set the owning side to null (unless already changed)
+            if ($iduser->getIduser() === $this) {
+                $iduser->setIduser(null);
+            }
+        }
+
+        return $this;
+    }
+}
